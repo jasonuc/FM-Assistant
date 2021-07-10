03d891a1846f1bed0fdb3f47674d84d1a314dfe6
jumahi <mrjasonu@gmail.com>
2021-07-10 22:48:38 +0100

    Deleted the useless lines from main.py

--- main.py
+++ main.py
@@ -2,33 +2,25 @@ import pandas as pd
 
 #Load in Fifa 19 CSV
 fifa = pd.read_csv('fifa19.csv')
-# print(fifa.head())
 
 #Start
 print("WELCOME TO FIFA MANAGER ASSISTANT")
 print("Note that this software uses Fifa 19 dataset.")
 
-# manager = str(input("WHAT IS YOUR NAME? ")).upper()
-manager = 'JCU'
+manager = str(input("WHAT IS YOUR NAME? ")).upper()
 
 print("\nWelcome to the player window search".upper())
-# p_position = str(input(
-# """
-# What position are you looking for:
-# Postions: CAM, CB, CDM, CF, CB, GK, LB, LCM, LCB LAM, LF, LM, LW, LWB, RB, RCM, RCB RAM, RF, RM, RW, RWB, ST
-# """
-# )).upper()
-# p_age = int(input("What is the highest age you're looking for: "))
-# p_overall = int(input("What is the least rating you would like(0-100): "))
-# p_potential = int(input("What is the least potential you would like(0-100): "))
-
-p_position = 'CB'
-p_age = 23
-p_overall = 75
-p_potential = 85
+p_position = str(input(
+"""
+What position are you looking for:
+Postions: CAM, CB, CDM, CF, CB, GK, LB, LCM, LCB LAM, LF, LM, LW, LWB, RB, RCM, RCB RAM, RF, RM, RW, RWB, ST
+"""
+)).upper()
+p_age = int(input("What is the highest age you're looking for: "))
+p_overall = int(input("What is the least rating you would like(0-100): "))
+p_potential = int(input("What is the least potential you would like(0-100): "))
 
 players = fifa[(fifa.Age <= p_age) & (fifa.Position == p_position) & (fifa.Overall >= p_overall) & (fifa.Potential >= p_potential)]
-# names = list(players['Name'])
 
 for index, row in players.iterrows():
     name = row["Name"]
